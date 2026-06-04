952b9864ac7abac8238efcaff3b1b027b2857198
zhaobohao-byte <12838490+zhaobohao0813@user.noreply.gitee.com>
2026-06-04 17:01:24 +0800

    新增ultra的配置和博哥的参考json

--- scripts/bvh_to_robot.py
+++ scripts/bvh_to_robot.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     
     parser.add_argument(
         "--robot",
-        choices=["unitree_g1", "unitree_g1_with_hands", "booster_t1", "stanford_toddy", "fourier_n1", "engineai_pm01", "pal_talos"],
+        choices=["unitree_g1", "unitree_g1_with_hands", "booster_t1", "stanford_toddy", "fourier_n1", "engineai_pm01", "pal_talos", "ultra"],
         default="unitree_g1",
     )
     
